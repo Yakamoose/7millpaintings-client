6c3306591e5a0a70818162bc90dd62d802708933
Eric J <xericx.johnson@gmail.com>
2018-06-26 17:27:44 -0700

    fixed REDUX extension issue

--- src/store.js
+++ src/store.js
@@ -11,6 +11,4 @@ export default createStore(
   combineReducers({
     millionReducer,
     form:formReducer
-  }, applyMiddleware(thunk)), defaultState
-+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+  }, applyMiddleware(thunk)));
